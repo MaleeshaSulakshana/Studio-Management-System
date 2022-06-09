636d695087b2f22329a0873d969eb4006c8f46e7
MaleeshaSulakshana <mpmsulakshana98@gmail.com>
2022-06-10 00:37:32 +0530

    remove files

--- server/studio/src/main/java/com/studio/studio/repository/EventRepository.java
+++
@@ -1,9 +0,0 @@
-package com.studio.studio.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.studio.studio.entity.Event;
-
-public interface EventRepository extends JpaRepository<Event, String>{
-
-}
